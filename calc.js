cca1523d6639aa8467903c91feeb76852ec6eef6
Wisani <matobelaw@gmail.com>
2022-05-13 17:02:06 +0200

    fixing calculator

--- calc.js
+++ calc.js
@@ -1,7 +1,134 @@
-// selectors
+class Calculator {
+  constructor(previousOperandTextElement, currentOperandTextElement) {
+    this.previousOperandTextElement = previousOperandTextElement;
+    this.currentOperandTextElement = currentOperandTextElement;
+    this.clear();
+  }
 
-const screen = document.querySelector("#screen");
-const btn = document.querySelectorAll(".btn");
+  clear() {
+    this.currentOperand = "";
+    this.previousOperand = "";
+    this.operation = undefined;
+  }
+
+  delete() {
+    this.currentOperand = this.currentOperand.toString().slice(0, -1);
+  }
+
+  appendNumber(number) {
+    if (number === "." && this.currentOperand.includes(".")) return;
+    this.currentOperand = this.currentOperand.toString() + number.toString();
+  }
+
+  chooseOperation(operation) {
+    if (this.currentOperand === "") return;
+    if (this.previousOperand !== "") {
+      this.compute();
+    }
+    this.operation = operation;
+    this.previousOperand = this.currentOperand;
+    this.currentOperand = "";
+  }
+
+  compute() {
+    let computation;
+    const prev = parseFloat(this.previousOperand);
+    const current = parseFloat(this.currentOperand);
+    if (isNaN(prev) || isNaN(current)) return;
+    switch (this.operation) {
+      case "+":
+        computation = prev + current;
+        break;
+      case "-":
+        computation = prev - current;
+        break;
+      case "*":
+        computation = prev * current;
+        break;
+      case "÷":
+        computation = prev / current;
+        break;
+      default:
+        return;
+    }
+    this.currentOperand = computation;
+    this.operation = undefined;
+    this.previousOperand = "";
+  }
+
+  getDisplayNumber(number) {
+    const stringNumber = number.toString();
+    const integerDigits = parseFloat(stringNumber.split(".")[0]);
+    const decimalDigits = stringNumber.split(".")[1];
+    let integerDisplay;
+    if (isNaN(integerDigits)) {
+      integerDisplay = "";
+    } else if (decimalDigits != null) {
+      return `${integerDisplay}.${decimalDigits}`;
+    } else {
+      return integerDisplay;
+    }
+  }
+
+  updateDisplay() {
+    this.currentOperandTextElement.innerText = this.getDisplayNumber(
+      this.currentOperand
+    );
+    if (this.operation != null) {
+      this.previousOperandTextElement.innerText = `${this.getDisplayNumber(
+        this.previousOperand
+      )} ${this.operation}`;
+    } else {
+      this.previousOperandTextElement.innerText = "";
+    }
+  }
+}
+
+const numberButtons = document.querySelectorAll("[data-number]");
+const operationButtons = document.querySelectorAll("[data-operation]");
+const equalsButton = document.querySelector("[eval]");
+const deleteButton = document.querySelector("[delete]");
+const allClearButton = document.querySelector("[ac]");
+const previousOperandTextElement = document.querySelector(
+  "[data-previous-operand]"
+);
+const currentOperandTextElement = document.querySelector(
+  "[data-current-operand]"
+);
+
+const calculator = new Calculator(
+  previousOperandTextElement,
+  currentOperandTextElement
+);
+
+numberButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    calculator.appendNumber(button.innerText);
+    calculator.updateDisplay();
+  });
+});
+
+operationButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    calculator.chooseOperation(button.innerText);
+    calculator.updateDisplay();
+  });
+});
+
+equalsButton.addEventListener("click", (button) => {
+  calculator.compute();
+  calculator.updateDisplay();
+});
+
+allClearButton.addEventListener("click", (button) => {
+  calculator.clear();
+  calculator.updateDisplay();
+});
+
+deleteButton.addEventListener("click", (button) => {
+  calculator.delete();
+  calculator.updateDisplay();
+});
 
 const ce = document.getElementById("ce");
 ce.addEventListener("click", backspace);
@@ -48,30 +175,51 @@ cub.addEventListener("click", Cubic);
 const fact = document.getElementById("fact");
 fact.addEventListener("click", factorial);
 
-function reset() {
-  screen.value = "";
-}
+// function reset() {
+//   screen.value = "";
+// }
 
 // const equal = document.getElementById("equal");
 // equal.addEventListener("click", calculate);
 
-function percentage() {
-  window.Function(screen.value * 100 + "%");
-}
+// const buttons = document.querySelectorAll('button');
+// const display = document.querySelector('.display');
+// buttons.forEach(function(button) {
+//   button.addEventListener('click', calculate);
+// });
+// function calculate(event) {
+//   const clickedButtonValue = event.target.value;
+//   if (clickedButtonValue === '=') {
 
-for (item of btn) {
-  item.addEventListener("click", (e) => {
-    let btntext = e.target.innerText;
+//     if (display.value !== '')
+//     {
+//       display.value = eval(display.value);
+//     }
+//   } else if (clickedButtonValue === 'C')
+//   {
+//     display.value = '';
+//   } else {
+//     display.value += clickedButtonValue;
+//   }
+// }
 
-    if (btntext == "x") {
-      btntext = "*";
-    }
-    if (btntext == "÷") {
-      btntext = "/";
-    }
-    screen.value += btntext;
-  });
-}
+// function percentage() {
+//   window.Function(screen.value * 100 + "%");
+// }
+
+// for (item of btn) {
+//   item.addEventListener("click", (e) => {
+//     let btntext = e.target.innerText;
+
+//     if (btntext == "x") {
+//       btntext = "*";
+//     }
+//     if (btntext == "÷") {
+//       btntext = "/";
+//     }
+//     screen.value += btntext;
+//   });
+// }
 
 function radi() {
   let radian = screen.value;
@@ -122,9 +270,8 @@ function sqrt() {
 }
 
 function pi() {
-  let py = screen.value;
-  let phy = py * 3.14159265359;
-  screen.value = phy;
+  let py = screen.value * 3.14159265359;
+  screen.value = py;
 }
 
 function euler() {
@@ -146,14 +293,14 @@ function factorial() {
   }
 }
 
-function backspace() {
-  screen.value = screen.value.substring(0, screen.value.length - 1);
-}
+// function backspace() {
+//   screen.value = screen.value.substring(0, screen.value.length - 1);
+// }
 
-function darkMode() {
-  let element = document.calculator;
-  element.classList.toggle("dark-mode");
-}
+// function darkMode() {
+//   let element = document.calculator;
+//   element.classList.toggle("dark-mode");
+// }
 
 // function calculate(){
 //   const parser = math.parser()
@@ -200,9 +347,7 @@ function darkMode() {
 // }
 
 function bodmas(){
-
-  let equation;
-
+let equation;
   for(let i =0; i < equation.length; i++){
     if(equation.contains("(") && !equation.contains(")")) {
       return SyntaxError;
@@ -212,4 +357,4 @@ function bodmas(){
     }
     }
 
-  }
+   }
