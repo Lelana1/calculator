15b6a73b268ea63cc899171ac87a736fc995328b
Wisani <matobelaw@gmail.com>
2022-05-17 09:53:28 +0200

    re-adding trig functionality

--- calc.js
+++ calc.js
@@ -8,23 +8,23 @@ ce.addEventListener("click", backspace);
 const ac = document.getElementById("ac");
 ac.addEventListener("click", reset);
 
-// const sine = document.getElementById("sine");
-// sine.addEventListener("click", sin);
+const sine = document.getElementById("sine");
+sine.addEventListener("click", sin);
 
-// const cosine = document.getElementById("cosine");
-// cosine.addEventListener("click", cos);
+const cosine = document.getElementById("cosine");
+cosine.addEventListener("click", cos);
 
-// const rad = document.getElementById("rad");
-// rad.addEventListener("click", radi);
+const rad = document.getElementById("rad");
+rad.addEventListener("click", radi);
 
-// const ln = document.getElementById("ln");
-// ln.addEventListener("click", lan);
+const ln = document.getElementById("ln");
+ln.addEventListener("click", lan);
 
-// const pie = document.getElementById("pie");
-// pie.addEventListener("click", pi);
+const pie = document.getElementById("pie");
+pie.addEventListener("click", pi);
 
-// const logart = document.getElementById("logart");
-// logart.addEventListener("click", log);
+const logart = document.getElementById("logart");
+logart.addEventListener("click", log);
 
 const sqroot = document.getElementById("sqroot");
 sqroot.addEventListener("click", sqrt);
@@ -66,21 +66,24 @@ operationButtons.forEach((button) => {
   });
 });
 
-const operations = {
-  sin: (x) => Math.sin(x),
-  cos: (x) => Math.cos(x),
-  tan: (x) => Math.tan(x),
-  lan: (x) => Math.log(x),
-  log: (x) => Math.log10(x),
-  euler: (x) => Math.E(x),
-};
+// const operations = {
+//   sin: (x) => Math.sin(x),
+//   cos: (x) => Math.cos(x),
+//   tan: (x) => Math.tan(x),
+//   lan: (x) => Math.log(x),
+//   log: (x) => Math.log10(x),
+//   euler: (x) => Math.E(x),
+// };
 
 operations["+"] = (numbers) =>
-  numbers.reduce((accumulated, current) => accumulated + current); //
-operations["-"] = (numbers) =>
+  numbers.reduce((accumulated, current) => accumulated + current);
+   //
+operations["-"] = (...numbers) =>
 numbers.reduce((accumulated, current) => accumulated - current, 0);
+
 operations["/"] = (numbers) =>
 numbers.reduce((accumulated, current) => accumulated / current, 0);
+
 operations["*"] = (numbers) =>
 numbers.reduce((accumulated, current) => accumulated * current, 1);
 
@@ -89,19 +92,6 @@ operations["%"] = (first, second) => (first / second) * 100 + "%";
 // const displayValue = [];
 // const getDisplayNumber = () =>
 //   displayValue.reduce((accumulated, current) => accumulated + "" + current, "");
-
-// function display(number) {
-//   const last = displayValue.pop();
-
-//   if (!isNaN(last) && !isNaN(number)) {
-//     displayValue.push(`${last}${number}`);
-//   } else {
-//     displayValue.push(last);
-//     displayValue.push(number);
-//   }
-
-//   screen.value = getDisplayNumber();
-// }
 
 const perc = document.getElementById("perc");
 perc.addEventListener("click", () => (screen.value += "%"));
@@ -139,7 +129,7 @@ function sqrt() {
     if (screen.value.includes("-")) {
     screen.value = operations["-"](numbers);
   }
-  if (screen.value.includes("÷")) {
+    if (screen.value.includes("÷")) {
     screen.value = operations["/"](numbers);
   }
   });
